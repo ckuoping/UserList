1a3dcb6e52fd4aef1cb4e0caa4254d91e1b0a837
Marvin <ckuoping@gmail.com>
2023-06-10 10:40:29 +0800

    add login and signup functions

--- src/app/component/login/login.component.ts
+++ src/app/component/login/login.component.ts
@@ -1,15 +1,42 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  loginForm !: FormGroup;
+  constructor(private formbuilder : FormBuilder,private http : HttpClient, private router : Router) { }
 
   ngOnInit(): void {
-  }
+    this.loginForm = this.formbuilder.group(
+      {    
+        email : [''],
+        password : ['']
+      }
 
+    )
+  }
+  logIn(){
+    this.http.get<any>("http://localhost:3000/signupUsers")
+    .subscribe(res=>{
+      const user = res.find((a:any)=>{
+        return a.email === this.loginForm.value.email && a.password === this.loginForm.value.password
+      });
+
+      if(user){
+        alert('login successfully');
+        this.loginForm.reset();
+        this.router.navigate(['dashboard']);
+      }
+      else{
+        alert('user not found');
+      }
+    },err=>{
+      alert('something went wrong');
+    })
+  }
 }
--- src/app/component/signup/signup.component.ts
+++ src/app/component/signup/signup.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
@@ -7,9 +9,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
-  constructor() { }
+  signupForm !: FormGroup;
+  constructor(private formbuilder : FormBuilder,private http : HttpClient, private router : Router) { }
 
   ngOnInit(): void {
+
+    this.signupForm = this.formbuilder.group(
+      {
+        fullname : [''],
+        email : [''],
+        password : [''],
+        mobile : ['']
+      }
+
+    )
+  }
+
+  signUp(){
+    this.http.post<any>("http://localhost:3000/signupUsers",this.signupForm.value)
+    .subscribe(res=>{
+      alert('Sign Up Successfully!');
+
+      this.signupForm.reset();
+      
+      this.router.navigate(['login']);
+    },err=>{
+      alert('Sign Up Fail!');
+    })
   }
 
 }
